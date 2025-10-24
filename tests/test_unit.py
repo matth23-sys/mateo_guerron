35debcc2d32d2ce0b2fb58ebec2585297908ee93
Mateo Guerron <mateo.guerron236@gmail.com>
2025-10-24 16:29:44 -0500

    Tercer commit: corrige tests, CI pasa correctamente

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -3,6 +3,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import add
 
-def test_add_should_fail_initially():
-    # Falla intencionalmente
-    assert add(2, 2) == 5
+def test_add_ok():
+    assert add(2, 2) == 4
+    assert add(-1, 3) == 2
